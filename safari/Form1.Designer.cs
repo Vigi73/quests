e05b674606149c030b32e2a60a5337b9c4cac751
Vigi73 <vigi73@mqil.ru>
2021-02-21 10:21:43 +0700

    first commit

---
+++ safari/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace safari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clm1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clm2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clm4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmBase = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmLocation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmBait = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clm3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbStatusText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Peru;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeight = 22;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clm1,
+            this.clm2,
+            this.clm4,
+            this.clmBase,
+            this.clmLocation,
+            this.clmBait,
+            this.clm3});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowHeadersWidth = 10;
+            this.dataGridView1.Size = new System.Drawing.Size(707, 252);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // clm1
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            this.clm1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.clm1.DividerWidth = 1;
+            this.clm1.HeaderText = "Рыба";
+            this.clm1.Name = "clm1";
+            this.clm1.ReadOnly = true;
+            this.clm1.Width = 60;
+            // 
+            // clm2
+            // 
+            this.clm2.HeaderText = "От";
+            this.clm2.Name = "clm2";
+            this.clm2.ReadOnly = true;
+            this.clm2.Width = 45;
+            // 
+            // clm4
+            // 
+            this.clm4.HeaderText = "До";
+            this.clm4.Name = "clm4";
+            this.clm4.ReadOnly = true;
+            this.clm4.Width = 47;
+            // 
+            // clmBase
+            // 
+            this.clmBase.HeaderText = "База";
+            this.clmBase.Name = "clmBase";
+            this.clmBase.ReadOnly = true;
+            this.clmBase.Width = 57;
+            // 
+            // clmLocation
+            // 
+            this.clmLocation.HeaderText = "Локация";
+            this.clmLocation.Name = "clmLocation";
+            this.clmLocation.ReadOnly = true;
+            this.clmLocation.Width = 76;
+            // 
+            // clmBait
+            // 
+            this.clmBait.HeaderText = "Наживка";
+            this.clmBait.Name = "clmBait";
+            this.clmBait.ReadOnly = true;
+            this.clmBait.Width = 78;
+            // 
+            // clm3
+            // 
+            this.clm3.HeaderText = "Статус вылова";
+            this.clm3.Name = "clm3";
+            this.clm3.Width = 88;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Leelawadee", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(706, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Найти вариант вылова";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbStatusText
+            // 
+            this.lbStatusText.AutoSize = true;
+            this.lbStatusText.Location = new System.Drawing.Point(705, 267);
+            this.lbStatusText.Name = "lbStatusText";
+            this.lbStatusText.Size = new System.Drawing.Size(13, 13);
+            this.lbStatusText.TabIndex = 2;
+            this.lbStatusText.Text = "0";
+            this.lbStatusText.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(730, 319);
+            this.Controls.Add(this.lbStatusText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Рыбное сафари";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clm4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmBase;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmLocation;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmBait;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clm3;
+        private System.Windows.Forms.Label lbStatusText;
+    }
+}
+
---
+++ safari/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace safari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (Clipboard.ContainsText() == true)
+            {
+                //Извлекаем (точнее копируем) его и сохраняем в переменную
+                string someText = Clipboard.GetText();
+
+                var tmp = someText.Split('\n');
+                //var nameFish = Regex.Split(tmp[0], "-?[0-9]+");
+                //MessageBox.Show(nameFish[0]);
+
+                var n = tmp.Length;
+                //Выводим показываем сообщение с текстом, скопированным из буфера обмена
+                for (var i = 0; i < n - 1; i++)
+                {
+                    var nameFish = Regex.Split(tmp[i].Trim(), "-?[0-9]+")[0].Trim();
+                    string pattern = @"(\d+(?:\.\d+)?)";
+                    Regex rgx = new Regex(pattern);
+                    var minWeight = tmp[i].Trim().Replace(",", ".");
+
+                    string whereFish = getBase1(nameFish);
+
+                    
+                    //MessageBox.Show(whereFish.ToString());
+                    
+
+                    dataGridView1.Rows.Add(); // Создаем пустую запись в таблице
+                    dataGridView1.Rows[i].Cells[0].Value = nameFish; // Пишем название рыбы
+                    dataGridView1.Rows[i].Cells[1].Value = rgx.Matches(minWeight)[0]; // Вес от
+                    dataGridView1.Rows[i].Cells[2].Value = rgx.Matches(minWeight)[1]; // Вес До
+                    dataGridView1.Rows[i].Cells[3].Value = whereFish.Split(':')[0]; // База
+                    dataGridView1.Rows[i].Cells[4].Value = whereFish.Split(':')[1]; // База
+                    dataGridView1.Rows[i].Cells[5].Value = whereFish.Split(':')[2]; // База
+                    //dataGridView1.Rows[i].Cells[3].Value = baseFish; // Вес До
+                    
+
+                }
+                
+
+            }
+            else
+            {
+                //Выводим сообщение о том, что в буфере обмена нет текста
+                MessageBox.Show(this, "В буфере обмена нет текста", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            lbStatusText.Text = (dataGridView1.Rows.Count - 1).ToString();
+
+
+
+        }
+
+        private string getBase1(string nameFish)
+        {
+            // Получаем где ловить
+           string result = String.Empty;
+
+            const string databaseName = @"DataBase.db";
+            SQLiteConnection connection =
+            new SQLiteConnection(string.Format("Data Source={0};", databaseName));
+            connection.Open();
+
+            const string databaseName2 = @"DataBase.db";
+            SQLiteConnection connection2 =
+            new SQLiteConnection(string.Format("Data Source={0};", databaseName2));
+            connection2.Open();
+
+
+
+            SQLiteCommand command = new SQLiteCommand($"SELECT BasesFishCont.CountFish, Fishes.Name, Bases.Name, BasesFishCont.MinWeight, BasesFishCont.MaxWeight, Locations.Name, Bait.Name FROM  Bait, Locations, Bases, Fishes, BasesFishCont  WHERE BasesFishCont.IDCatchBait = Bait.ID AND BasesFishCont.IDCatchyLocation = Locations.ID AND BasesFishCont.IDBases = Bases.ID AND BasesFishCont.IDFish = Fishes.ID AND Fishes.Name='{nameFish}'", connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            SQLiteCommand command2 = new SQLiteCommand($"SELECT BasesFishCont.CountFish, Fishes.Name, Bases.Name, BasesFishCont.MinWeight, BasesFishCont.MaxWeight, Locations.Name, Bait.Name FROM  Bait, Locations, Bases, Fishes, BasesFishCont  WHERE BasesFishCont.IDCatchBait = Bait.ID AND BasesFishCont.IDCatchyLocation = Locations.ID AND BasesFishCont.IDBases = Bases.ID AND BasesFishCont.IDFish = Fishes.ID AND Fishes.Name='{nameFish}'", connection2);
+            SQLiteDataReader reader2 = command2.ExecuteReader();
+
+            int maxСountFish = 0;
+
+            foreach (DbDataRecord record in reader)
+            {
+
+                if (maxСountFish < int.Parse($"{record[0]}"))
+                    maxСountFish = int.Parse($"{record[0]}");
+            }
+            
+            connection.Close();
+
+            // 0 - Колличесто
+            // 1 - Рыба
+            // 2 - База
+            // 3 - min
+            // 4 - max
+            // 5 - Локация
+            // 6 - Наживка
+
+            foreach (DbDataRecord record in reader2)
+                if (int.Parse($"{record[0]}") == maxСountFish)
+                {
+                    result = $"{record[2]}:{record[5]}:{record[6]}";
+                }
+
+            connection2.Close();
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbStatusText.Text = "0";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //
+        }
+
+        // Получаем где ловить
+
+
+    }
+}
+
